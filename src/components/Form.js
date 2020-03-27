49f717e8f3ab0492c8558f537c0f87609c6963b5
Danial Hadavi <danialhadavi@gmail.com>
2020-03-27 15:58:02 -0400

    added some styling for fun

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import * as yup from "yup";
+import { Link } from "react-router-dom";
 
 const formSchema = yup.object().shape({
   name: yup.string().min(2, "Name should be at least 2 characters"),
@@ -86,76 +87,91 @@ export default function Form() {
   };
 
   return (
-    <form onSubmit={formSubmit}>
-      <label htmlFor="name">
-        Your Name
-        <input
-          type="text"
-          name="name"
-          value={formState.name}
-          onChange={inputChange}
-        />
-        {errors.name.length > 0 ? <p className="error">{errors.name}</p> : null}
-      </label>
+    <>
+      <nav>
+        <ul>
+          <Link to="/">
+            {" "}
+            <li>Home</li>
+          </Link>
+          <li>Menu</li>
+          <li>Services</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+      <form onSubmit={formSubmit}>
+        <label htmlFor="name">
+          Your Name
+          <input
+            type="text"
+            name="name"
+            value={formState.name}
+            onChange={inputChange}
+          />
+          {errors.name.length > 0 ? (
+            <p className="error">{errors.name}</p>
+          ) : null}
+        </label>
 
-      <label htmlFor="special">
-        Special instructions
-        <textarea
-          name="special"
-          value={formState.special}
-          onChange={inputChange}
-        />
-      </label>
-      <label htmlFor="size">
-        Pizza Size
-        <select id="size" name="size" onChange={inputChange}>
-          <option value="small">small</option>
-          <option value="medium">medium</option>
-          <option value="large">large</option>
-          <option value="extra-large">extra-large</option>
-        </select>
-      </label>
-      <label htmlFor="toppings" className="toppings">
-        <input
-          type="checkbox"
-          name="pepperoni"
-          checked={formState.pepperoni}
-          onChange={inputChange}
-        />
-        pepperoni
-      </label>
-      <label htmlFor="toppings" className="toppings">
-        <input
-          type="checkbox"
-          name="olives"
-          checked={formState.olives}
-          onChange={inputChange}
-        />
-        olives
-      </label>
-      <label htmlFor="toppings" className="toppings">
-        <input
-          type="checkbox"
-          name="bacon"
-          checked={formState.bacon}
-          onChange={inputChange}
-        />
-        bacon
-      </label>
-      <label htmlFor="toppings" className="toppings">
-        <input
-          type="checkbox"
-          name="sausage"
-          checked={formState.sausage}
-          onChange={inputChange}
-        />
-        sausage
-      </label>
+        <label htmlFor="special">
+          Special instructions
+          <textarea
+            name="special"
+            value={formState.special}
+            onChange={inputChange}
+          />
+        </label>
+        <label htmlFor="size">
+          Pizza Size
+          <select id="size" name="size" onChange={inputChange}>
+            <option value="small">small</option>
+            <option value="medium">medium</option>
+            <option value="large">large</option>
+            <option value="extra-large">extra-large</option>
+          </select>
+        </label>
+        <label htmlFor="toppings" className="toppings">
+          <input
+            type="checkbox"
+            name="pepperoni"
+            checked={formState.pepperoni}
+            onChange={inputChange}
+          />
+          pepperoni
+        </label>
+        <label htmlFor="toppings" className="toppings">
+          <input
+            type="checkbox"
+            name="olives"
+            checked={formState.olives}
+            onChange={inputChange}
+          />
+          olives
+        </label>
+        <label htmlFor="toppings" className="toppings">
+          <input
+            type="checkbox"
+            name="bacon"
+            checked={formState.bacon}
+            onChange={inputChange}
+          />
+          bacon
+        </label>
+        <label htmlFor="toppings" className="toppings">
+          <input
+            type="checkbox"
+            name="sausage"
+            checked={formState.sausage}
+            onChange={inputChange}
+          />
+          sausage
+        </label>
 
-      <pre>{JSON.stringify(post, null, 2)}</pre>
-      <button name="submit" disabled={buttonDisabled}>
-        Order
-      </button>
-    </form>
+        <pre>{JSON.stringify(post, null, 2)}</pre>
+        <button name="submit" disabled={buttonDisabled}>
+          Order
+        </button>
+      </form>
+    </>
   );
 }
